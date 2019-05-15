9cd5f01d3ed05506b1b5d8cb20759e911296969f
Ismail Brkic <brkicismail@gmail.com>
2019-05-15 10:21:21 +0200

    Goal Class Done

--- src/ba/bildit/DTO/Goal.java
+++ src/ba/bildit/DTO/Goal.java
@@ -2,4 +2,41 @@ package ba.bildit.DTO;
 
 public class Goal {
 
+	private String goalName;
+	private String goalText;
+	private boolean completed = false;
+	
+	public Goal() {
+		
+	}
+	
+	public Goal(String goalName, String goalText) {
+		this.goalName = goalName;
+		this.goalText = goalText;
+	}
+	
+	
+	public String getGoalName() {
+		return goalName;
+	}
+	
+	public void setGoalName(String goalName) {
+		this.goalName = goalName;
+	}
+	public String getGoalText() {
+		return goalText;
+	}
+	public void setGoalText(String goalText) {
+		this.goalText = goalText;
+	}
+	public boolean isCompleted() {
+		return completed;
+	}
+	public void setCompleted(boolean completed) {
+		this.completed = completed;
+	}
+	
+	
+	
+	
 }
