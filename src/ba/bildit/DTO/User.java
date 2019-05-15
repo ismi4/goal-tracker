3e4a181975639ab4b4a40de985825161b7bc6b38
Ismail Brkic <brkicismail@gmail.com>
2019-05-15 10:48:21 +0200

    User and UserBuilder classes done

--- src/ba/bildit/DTO/User.java
+++ src/ba/bildit/DTO/User.java
@@ -2,4 +2,55 @@ package ba.bildit.DTO;
 
 public class User {
 
+	private Integer id;
+	private String username;
+	private String name;
+	private String surname;
+	private GoalList listOfGoals;
+	
+	User() {
+		
+	}
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getSurname() {
+		return surname;
+	}
+	
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	
+	public GoalList getListOfGoals() {
+		return listOfGoals;
+	}
+	
+	public void setListOfGoals(GoalList listOfGoals) {
+		this.listOfGoals = listOfGoals;
+	}
+	
 }
---
+++ src/ba/bildit/DTO/UserBuilder.java
@@ -0,0 +1,45 @@
+package ba.bildit.DTO;
+
+public class UserBuilder {
+
+	private Integer id;
+	private String username;
+	private String name;
+	private String surname;
+	private GoalList listOfGoals;
+	
+	public UserBuilder(Integer id) {
+		this.id = id;
+	}
+	
+	public UserBuilder withUsername(String username) {
+		this.username = username;
+		return this;
+	}
+	
+	public UserBuilder withName(String name) {
+		this.name = name;
+		return this;
+	}
+	
+	public UserBuilder withSurname(String surname) {
+		this.surname = surname;
+		return this;
+	}
+	
+	public UserBuilder withListOfGoals(GoalList listOfGoals) {
+		this.listOfGoals = listOfGoals;
+		return this;
+	}
+	
+	 public User build(){
+         User user = new User();  
+         user.setId(this.id);
+         user.setUsername(this.username);
+         user.setName(this.name);
+         user.setSurname(this.surname);
+         user.setListOfGoals(this.listOfGoals);
+         return user;
+     }
+	
+}
