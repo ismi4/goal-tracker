d31db423e5cc7bf710f211c1c55ea8d0626dba54
Ismail Brkic <brkicismail@gmail.com>
2019-05-18 11:40:48 +0200

    RegistrationProcess Done

--- src/ba/bildit/BO/RegistrationProcess.java
+++ src/ba/bildit/BO/RegistrationProcess.java
@@ -1,9 +1,58 @@
 package ba.bildit.BO;
 
+import java.sql.SQLException;
+
+import ba.bildit.DAO.UserDAOImplementation;
+import ba.bildit.UI.InitialMenu;
+import ba.bildit.main.Main;
+
 public class RegistrationProcess {
 
-	public static void registrationProcess() {
+	public static void registrationProcess() throws SQLException {
+	
+		System.out.println("Enter username:");
+		String username = Main.input.next();
+		
+		if (doesUserExists(username)) {
+			System.out.println("That username is already taken!");
+			InitialMenu.initialMenu();
+		}
+		
+		System.out.println("Enter your name: ");
+		String name = Main.input.next();
+
+		System.out.println("Enter your surname: ");
+		String surname = Main.input.next();
+		
+		System.out.println("Enter your password: ");
+		String password = Main.input.next();
+		
+		createUser(username, name, surname, password);
+		System.out.println("The user has been successfully created!");
+		
+		InitialMenu.initialMenu();
+	}
+	
+	
+	public static boolean doesUserExists(String username) throws SQLException {
+		
+		UserDAOImplementation accessUsers = new UserDAOImplementation();
+		
+		if (accessUsers.getUser(username) == null)
+			return false;
+		
+		return true;
 		
 	}
 	
-}
+	public static void createUser(String username, String name, String surname, String password) throws SQLException {
+		
+		UserDAOImplementation accessUsers = new UserDAOImplementation();
+		
+		accessUsers.addUser(username, name, surname, password);
+		
+	}
+		
+	}
+	
+
